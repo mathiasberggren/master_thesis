d7233251de271fc6fa3f91ce187f8cbf4bd9cf3d
Berggren <mathias.berggren@siemens.com>
2020-09-09 12:14:15 +0200

    Update for data-fetcher script

--- Programmering/data-handling/data-fetcher.py
+++ Programmering/data-handling/data-fetcher.py
@@ -3,22 +3,28 @@ import os
 from shutil import copyfile
 from progress.bar import IncrementalBar
 
+# ------ Project specifics ------ 
 source_dir = '..\..\..\..\GabrielLindman\Summer2020\HEEDS_final\HEEDS_dataForML\HEEDS_v6_Study_3\HEEDS_0'
 dest_dir = '.\ML-data\\'
 missing_files = 'missing_files.txt'
-# files to fetch
-files = ['exporter.inp', 'Disc_MI_steady_state.dat', 'Disc_MI_steady_state.odb', 'SMax_CAX6.txt', 'SMax_CAX8.txt']
+files = ['exporter.inp', 'Disc_MI_steady_state.dat', 
+            'Disc_MI_steady_state.odb', 'SMax_CAX6.txt', 'SMax_CAX8.txt']
 
-# ------ Project specific ------ #
 f_design_variables = '\design_variable_and_parameter_values'
-copyfile(source_dir + '\\Design1\\' + f_design_variables, dest_dir + f_design_variables)
-# ------------------------------ #
+copyfile(source_dir + '\\Design1\\' + f_design_variables, dest_dir + \
+            f_design_variables)
+# ------------------------------ 
 
 # Progress Bar
-bar = IncrementalBar('Fetching files', max=len(os.listdir(source_dir)))
+bar = IncrementalBar('  Fetching files', 
+        max=len(os.listdir(source_dir))*len(files),\
+        suffix='%(index)4i/%(max)i files processed : estimated time left '\
+        + '%(eta_td)s : %(percent).1f%%')
+
+# Avoid extra \n
+first = True
 
 for _map in os.listdir(source_dir):
-    # print(f"Copying source_dir: {_map}")
     try: 
         os.mkdir(dest_dir + _map)
     except OSError:
@@ -27,17 +33,22 @@ for _map in os.listdir(source_dir):
                 out.write(_map + "\n")
 
     for f in files:
+
         try: 
             source = f'{source_dir}\\{_map}\\abaqus\\{f}'
             destination = dest_dir + _map + '\\' + f
             copyfile(source, destination)
         except: 
             with open(missing_files, 'a') as out:
-                out.write(destination + "\n")
+                if (first):
+                    first = False
+                else: 
+                    out.write("\n")
+                out.write(f'{_map}\\abaqus\\{f}')
 
-    bar.next()
+        bar.next()
 
 bar.finish()
 
 if(os.path.isfile(missing_files)):
-    print(f"Some files are missing, for more information see {missing_files}")
+    print(f"Some files couldn't be loaded, for information see {missing_files}")
